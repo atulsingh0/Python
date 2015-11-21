d2f23668ef058fd5238b8ca95e3143144133c3b8
Atul Singh <victimizeit@gmail.com>
2015-11-21 15:00:18 +0530

    Create python1_syntax.py

---
+++ python1_syntax.py
@@ -0,0 +1,63 @@
+
+# Welcome to Python Learning
+# WWW.DATAGENX.NET
+
+print "Hello! Welcome to Python World!"
+
+# variables
+# variable can hold any type of value ( int, float or string )
+myName = "Atul"   # string data type
+myAge = 25        # integer data type
+float_no = 2.0    # float data type
+myBool = True     # boolean data type
+
+# you can print your variable with print command
+print myName      # o/p -> Atul
+print myAge       # o/p -> 25
+print float_no    # o/p -> 2.0
+print myBool
+
+# Simple math in python
+print 2 + 3.4      #  5.4 
+print 4 + 5        #  9
+
+print 4.5 - 2      #  2.5
+print 4.22 - 1.45  #  2.77
+print 9.454 - 10   #  -0.546
+
+print 2 * 4        #  8
+print 3.2 * 4      #  12.8 
+print -3 * 3       #  -9
+
+print 25 / 5       #  5
+print 4.2 / 2      #  2.1
+print 3 / 2        #  1 , strange !! because it is a integer division, so 
+                   #  python trimmed the decimal part ( in python 2.x  version int/int = int )
+print 3 / 2        # 1.5  in Python 3.x or higher
+print 3 // 2       # 1    in python 3.x or higher to achive python 2.x int divesion functionality
+print 3.0 / 2      # 1.5  
+print 3 / 2.0      # 1.5
+
+print 3 ** 2       # 9   power function
+print 4 ** 0.5     # 2.0  sqre root
+print 4 ** -2      # 0.0625
+
+print 3 % 2        # 1
+
+
+# Example calculation
+# calculate the simple interest   SI = P *  R * T / 100
+# where P - Pricipal amount, R - Rate of Interest, T - Tenure, 
+
+P = 10000
+R = 8.5
+T = 5
+SI = P * R * T / 100
+print SI
+
+
+# Thanks
+# Atul Singh
+# You can drop me a mail at below address for any queries or help
+# atul@datagenx.net
+
