786b3527a3ba52efb86d8c53c8eb5b8188ad9247
Atul Singh <victimizeit@gmail.com>
2015-11-21 15:32:10 +0530

    Create P1_MultiplesOf3And5.py

---
+++ Problems/P1_MultiplesOf3And5.py
@@ -0,0 +1,17 @@
+
+
+print "Problem 1"
+print "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23."
+print "Find the sum of all the multiples of 3 or 5 below 1000."
+
+No = int(raw_input("Enter a integer :")
+
+for i in range(No):
+  if i%3 == 0 or i%5 == 0:
+    print i,
+    
+
+
+print "Thanks"
+print "Atul Singh"
+print "www.datagenx.net"
