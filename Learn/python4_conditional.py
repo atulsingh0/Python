9455a495c8c70c5d7f5990086068bb027c4fc873
Atul Singh <victimizeit@gmail.com>
2015-11-24 11:21:28 +0530

    Create python4_conditional.py

---
+++ Learn/python4_conditional.py
@@ -0,0 +1,41 @@
+
+
+# Atul Singh
+# www.datagenx.net
+
+print "Python Points 4 - Conditional"
+
+var = 45
+
+# If Condition
+if var > 34 :
+	print "var is greater than 34"
+
+
+# If Else Condition
+if var > 63 :
+	print "var is greater than 34"
+else :
+	print "lesser than 63"
+
+
+# If Elif Else Condition
+if var > 63 :
+	print "We are in If Condition"
+elif var == 45 :
+	print "We are in elif Condition"
+else :
+	print "We are in Else Condition"
+
+
+# If one liner 
+if var < 65 : print "If one liner"
+
+
+# If Else one liner
+print( "Condition True" if var < 65 else "Condition False")
+
+
+# Atul Singh
+# www.datagenx.net
+
