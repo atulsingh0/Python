fdbdac1c8b99b84d4934bfdbb3b2656dfb459bdc
Atul Singh <victimizeit@gmail.com>
2016-03-31 18:52:28 +0530

    Update jupyter_windows.bat

--- tips/jupyter_windows.bat
+++ tips/jupyter_windows.bat
@@ -2,7 +2,7 @@
 # this script will ease the jupyter access to particular dir
 
 
-# for windows we can use below 6-9 lines 
+rem for windows we can use below 6-9 lines 
 rem -- start_jupyter_notebook_here.bat ---
 cd C:\Users\atul.singh\learn
 jupyter notebook 
@@ -10,13 +10,11 @@ pause
 
 
 
-# for linux use below line after removing # char
-# echo "starting the jupyter notebook"
-# cd /dir/path
-# jupyter notebook 
+rem for linux use below line after removing 'rem' word
+rem echo "starting the jupyter notebook"
+rem cd /dir/path
+rem jupyter notebook 
 
 
-# thanks
-# Atul Singh
-# www.datagenx.net
+
 
