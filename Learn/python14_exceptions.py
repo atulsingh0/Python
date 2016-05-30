a89c2be83f205ce6d4e9494324823773aa9bcc6a
Atul Singh <victimizeit@gmail.com>
2016-05-30 15:19:59 +0530

    Update python14_exceptions.py

--- Learn/python14_exceptions.py
+++ Learn/python14_exceptions.py
@@ -2,8 +2,13 @@
 
 # Exceptions
 
+import sys
+
 var1 = 6
 
+
+# Multiple Except Clauses
+print "\ntry1"
 try:
 	var2 = raw_input("Enter a value : ")
 	print var1/int(var2)
@@ -18,9 +23,9 @@ except KeyboardInterrupt:
 except:
 	print "\nUnknown exeception"
 	
-
+print "\ntry2"
 # Raising a exeception
-
+# Cleanup try...finally
 try:
 	raise ZeroDivisionError ("Somehitng")
 except ZeroDivisionError:
@@ -28,4 +33,27 @@ except ZeroDivisionError:
 finally:
 	print "I've learnt about exeception"
 	
+print "\ntry3"
+# Combining Exceptions into one	
+# Customized printing
+try:
+	var2 = raw_input("Enter a value : ")
+	print var1/int(var2)
+except ZeroDivisionError as (errno, err):
+	print "You can not deivide by zero",errno
+except (TypeError, ValueError):
+	print "There is Type or Value error"
+except KeyboardInterrupt:
+	print "\nCancel"
+except:
+	print "\nUnknown exeception"	
+	
+	
+print "\ntry4"
+try:
+	print 4/0
+except:
+	e = sys.exc_info()[1]
+	f = sys.exc_info()[0]
+	print f,e
 	
