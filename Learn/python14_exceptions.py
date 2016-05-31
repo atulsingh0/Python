f9bbe9e3dc81459f6255ff99fad7a8ce2c833969
Atul Singh <victimizeit@gmail.com>
2016-05-31 12:43:12 +0530

    Update python14_exceptions.py

--- Learn/python14_exceptions.py
+++ Learn/python14_exceptions.py
@@ -57,3 +57,15 @@ except:
 	f = sys.exc_info()[0]
 	print f,e
 	
+print "\ntry5"	
+try:
+    f = open('myfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)
+except ValueError:
+    print "Could not convert data to an integer."
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
