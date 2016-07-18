ae964a881462ca72cd42c52c5c3fca8a2a6c319c
Atul Singh <victimizeit@gmail.com>
2016-07-18 16:05:25 +0530

    Create getMSSQLdata.py

---
+++ Learn/getMSSQLdata.py
@@ -0,0 +1,41 @@
+
+# connect to MS SQL server
+# fetch the data from table and store it into csv file
+
+import pymssql  
+import sys
+import csv
+    
+SRVR='127.0.0.1'
+USR='dsusr'	
+PWD='Passw0rd'
+DB='sample'
+
+try:
+	conn = pymssql.connect(server=SRVR, user=USR, password=PWD, database=DB)
+except :
+	print "Error", sys.exc_info()[0]
+	exit()
+
+cursor = conn.cursor()  
+cursor.execute("select course_id, title, dept_name, credits from [dbo].[course]")
+row = cursor.fetchone()  
+
+'''
+with open('output_file.csv', 'wb') as fout:
+    writer = csv.writer(fout)
+    writer.writerow([ i[0] for i in cursor.description ]) # heading row
+    writer.writerows(cursor.fetchall())
+'''
+csv_out = open('mycsv.csv', 'wb')
+mywriter = csv.writer(csv_out)
+mywriter.writerow([ i[0] for i in cursor.description ]) # heading row
+
+while row:  
+	print row
+	mywriter.writerow(row)
+	row = cursor.fetchone()
+
+csv_out.close()	
+
+
