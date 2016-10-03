fef245ba9fd9cef8a2d66ba38822d82ffd7c4cec
Atul Singh <victimizeit@gmail.com>
2016-10-03 11:07:02 +0530

    Rename flattenJson.py to flattenJson2.py

---
+++ Learn/flattenJson2.py
@@ -0,0 +1,51 @@
+
+
+import json
+import csv
+import os
+import sys
+
+if len(sys.argv)<2:
+	#print("Passed arguement is not correct")
+    #print("Usgae: python flattenJson.py <input-dir> <output-dir>")
+	print "wrong"
+	exit()
+elif os.path.isdir(sys.argv[1]) is False:
+    #print("Input Directory path is not exists")
+    #print("Usgae: python flattenJson.py <input-dir> <output-file_absolute_path>")
+	print "wrong"
+	exit()
+else:
+	print "success"
+    #print("success")
+	
+
+indir = sys.argv[1]
+outfile = sys.argv[2]
+all_files = [os.path.join(indir,file) for file in os.listdir(indir) if file.endswith(".txt")]
+fcsv = open(outfile,"w")
+lineno=0
+for file in all_files:
+	fjson = open(file, "r")
+	csvs = csv.writer(fcsv, delimiter='|',lineterminator='\n')
+	for line in fjson:
+		data = json.loads(line)
+		csv_line = []
+		csv_header = []
+		lineno=lineno+1
+		for k,v in data.items():
+			if isinstance(v,dict):
+				csv_line.extend(json.loads(json.dumps(v)).values())
+				if lineno==1:
+					csv_header.extend([k+"."+key for key in json.loads(json.dumps(v)).keys()])
+			else:
+				csv_line.append(v)
+				if lineno==1:
+					csv_header.append(k)
+		if lineno==1:
+			csvs.writerow(csv_header)
+		csv_line = ['NULL' if len(str(x))<1 else x for x in csv_line ]
+		csvs.writerow(csv_line)
+    
+	fjson.close()
+fcsv.close()
