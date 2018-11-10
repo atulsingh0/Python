3f9356f67b9d8d6448afeabb5178cd98bdf34743
Atul Singh <victimizeit@gmail.com>
2018-11-10 13:30:49 -0500

    Update WebService_HealthCheck.py

--- Learn/WebService_HealthCheck.py
+++ Learn/WebService_HealthCheck.py
@@ -17,7 +17,7 @@ import sys
 
 if len(sys.argv) < 2 :
 	print("Usage: python <ENV> [LineNo]")
-	print("ENV : QA1, QA2, IST, PROD")
+	print("ENV : QA, IST")
 	sys.exit(1);
 elif len(sys.argv)==2 :
 	env=sys.argv[1]
@@ -30,37 +30,25 @@ def env_conf(env):
 	"returning env conf file"
 	return (env+'.cnf', 'outok'+env, 'outerr'+env)
 
-if env=='QA2':
-	inputfile='QA2.cnf'
-	outputokfile='QA2.out'
-	outputerrorfile='QA2.err'
-	cert_file_path = "nonPrd.cer"
-	key_file_path = "nonPrd_keys.key"
-elif env=='QA1':
-	inputfile='QA1.cnf'
-	outputokfile='QA1.out'
-	outputerrorfile='QA1.err'
+if env=='QA':
+	inputfile='WebService_HealthCheck_QA.config'
+	outputokfile='WebService_HealthCheck_QA.out'
+	outputerrorfile='WebService_HealthCheck_QA.err'
 	cert_file_path = "nonPrd.cer"
 	key_file_path = "nonPrd_keys.key"
 elif env=='IST':
-	inputfile='IST.cnf'
-	outputokfile='IST.out'
-	outputerrorfile='IST.err'
-	cert_file_path = "nonPrd.cer"
-	key_file_path = "nonPrd_keys.key"
-elif env=='PROD':
-	inputfile='PROD.cnf'
-	outputokfile='PROD.out'
-	outputerrorfile='PROD.err'
+	inputfile='WebService_HealthCheck_IST.cnf'
+	outputokfile='WebService_HealthCheck_IST.out'
+	outputerrorfile='WebService_HealthCheck_IST.err'
 	cert_file_path = "nonPrd.cer"
 	key_file_path = "nonPrd_keys.key"
 else:
 	print("Enter the correct env value")
 	print("Usage: python <ENV> [LineNo]")
-	print("ENV : QA1, QA2, IST, PROD")
+	print("ENV : QA, IST")
 	sys.exit(2);	
 
-urlTemplate = Template('https://zoeken.kvk.nl/search.ashx?handelsnaam=&kvknummer=$kvk_number&straat=&postcode=&huisnummer=&plaats=&hoofdvestiging=true&rechtspersoon=true&nevenvestiging=true&zoekvervallen=0&zoekuitgeschreven=0&start=0&initial=0&searchfield=uitgebreidzoeken')
+urlTemplate = Template('https://dummy_url')
 
 headers = {'Content-Type':'text/xml;charset=UTF-8'}
 
@@ -76,7 +64,6 @@ def fire_rest_post_request(url, timeout, inp):
 	start_time = time.time()
 	cert=(cert_file_path,key_file_path)
 	response=requests.post(url, timeout=timeout,headers=headers, data=inp, cert=cert, verify=False)
-	#response=requests.post(url, timeout=timeout,headers=headers, data=inp) #, verify='sITDClient_SHA2_Password1.pfx')
 	end_time=time.time() - start_time
 	data=response.json()
 	return {'responsetime':end_time,'response':response, 'data':data}
