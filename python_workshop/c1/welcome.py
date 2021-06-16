d78b370ccd5a5d72747cbe1a19f109a35c0783bb
atulsingh0 <victimizeit@gmail.com>
2021-06-16 10:44:11 -0400

    class1

---
+++ python_workshop/c1/welcome.py
@@ -0,0 +1,18 @@
+# this is a comment 
+# Blank line is skipped by interpretter
+
+
+# This is a welcome program 
+print("Hi, Welcome to Python!")  # This will print Hi
+print("What is your name?")  # Pass any String value
+fname = input()   # input is a func which take user input 
+
+print("It's nice to meet you " + fname)
+print("What year is this?")   # Pass Any Interger and Float value
+yr = float(input())
+
+print("Next Year will be " + str(yr + 1)) 
+
+
+print("Byeeeeeeee")
+
